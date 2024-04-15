f1a85ba49939d768c839918f028936e8c7ccb070
ssdgdf <167066155+ssdgdf@users.noreply.github.com>
2024-04-15 16:39:53 +0900

    Add files via upload

---
+++ day01/input.py
@@ -0,0 +1,17 @@
+#print : 글 나오도록 하는 기능
+# input : 글을 입력 하는 기능
+
+# drama = input("당신의 최애 드라마는?")
+# print(f"당신의 최애 드라마는{drama}이군요!!")
+#
+# name = input("당신의 이름은?")
+# print(f"당신의 이름은{name}이군요")
+
+name = input("당신의 이름은?")
+age = input("당신의 나이는?")
+color = input("당신이 좋아하는 색은?")
+print(f"제이름은{name}이고 나이는{age}이며 좋아하는 색은{color}입니다")
+
+day=input("오늘의 날짜는?")
+work = input("오늘의 계획은?")
+print(f"오늘은{day}이고 계획은{work}입니다")
---
+++ day01/print.py
@@ -0,0 +1,2 @@
+print("hello world")
+print("안녕하세요 저는 고주영이고 나이는 24살입니다")
